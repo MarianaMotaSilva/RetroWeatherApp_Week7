7f8f36cfd373dbe69ca63a005b98dd61903864e8
Mariana <mariana.ssousasilva@gmail.com>
2024-01-30 01:00:25 +0100

    Corrections HTML, CSS, JS

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-function formatDate(date) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let currentDay = days[date.getDay()];
-  let currentHour = date.getHours();
-  let currentMinutes = date.getMinutes();
-  if (currentMinutes < 10) {
-    currentMinutes = `0${currentMinutes}`;
-  }
-
-  if (currentHour < 10) {
-    currentHour = `0${currentHour}`;
-  }
-  let nowDate = `${currentDay} ${currentHour}:${currentMinutes}`;
-  return nowDate;
-}
-
-let time = document.querySelector("#date-now");
-time.innerHTML = formatDate(new Date());
-
 function displayTemperature(response) {
   let city = response.data.city;
   let currentCity = document.querySelector("#current-city");
@@ -33,18 +6,54 @@ function displayTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
   let currentTemp = document.querySelector(".current-temperature-value");
   currentTemp.innerHTML = temperature;
+
+  let humidity = response.data.temperature.humidity;
+  let currentHumidity = document.querySelector("#humidity");
+  currentHumidity.innerHTML = humidity;
+
+  let windSpeed = response.data.wind.speed;
+  let currentWindSpeed = document.querySelector("#wind-speed");
+  currentWindSpeed.innerHTML = windSpeed;
+
+  let description = response.data.condition.description;
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = description;
+
+  let iconUrl = response.data.condition.icon_url;
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${iconUrl}" class="current-temperature-icon" />`;
+
+  displayLocalTime();
+}
+
+function displayLocalTime() {
+  let currentDate = new Date();
+  let options = {
+    weekday: "long",
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+  };
+  let localTimeString = currentDate.toLocaleString("en-US", options);
+  document.querySelector("#date-now").textContent = localTimeString;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  showWeatherInfoForCity("Barcelona");
+});
+
+function showWeatherInfoForCity(cityName) {
+  let apiKey = "00a3d0oe3b36e11ff9bf2a3fd4b8t806";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function showNewCityInfo(event) {
   event.preventDefault();
-  let newCityname = "Paris";
   let inputCity = document.querySelector("#search-input");
-  newCityname = inputCity.value.trim();
-
-  let apiKey = "00a3d0oe3b36e11ff9bf2a3fd4b8t806";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${newCityname}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayTemperature);
+  let cityName = inputCity.value.trim();
+  showWeatherInfoForCity(cityName);
 }
 
 let form = document.querySelector("#city-form");
